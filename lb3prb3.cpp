195a482b2600adc23b1d9475d0e3529528b49dc5
Susan Sapkota <166668709+sapkota986@users.noreply.github.com>
2024-08-02 07:41:37 +0545

    Update lb3prb3.cpp

--- lb3prb3.cpp
+++ lb3prb3.cpp
@@ -1,62 +1,71 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-class Cartesian;
-class Polar {
-    float radius,angle; 
+
+class Polar
+{
+    float radius, angle;
 
 public:
-    Polar() {
-        radius=0.0;
-        angle=0.0;
-    }
-    Polar (float r,float a){
-        radius=r;
-        angle=a;
-    }
-    operator Cartesian();
-
-    void display()  {
-        cout << "Polar Coordinates: (" << radius << ", " << angle << " degrees)" << endl;
+polar(){
+    radius = 0.0;
+     angle = 0.0;
+}
+    Polar(float r,float a)
+    {
+        radius = r;
+        angle = a;
     }
 
-
-   
+    void display()
+    {
+        cout << "Polar Coordinates: (" << radius << ", " << angle << ")" << endl;
+    }
 };
 
-class Cartesian {
+class Cartesian
+{
     float xco, yco;
 
 public:
-    Cartesian() {
-        xco=0.0;
-        yco=0.0;
+Cartesian(){
+    xco=0.0;
+    yco=0.0;
+}
+    Cartesian(float x, float y)
+    {
+        xco = x;
+        yco = y;
+    }
 
+    void input()
+    {
+        cout << "Enter xco: ";
+        cin >> xco;
+        cout << "Enter yco: ";
+        cin >> yco;
     }
-    Cartesian(float x,float y){
-        xco=x;
-        yco=y;
+
+    operator Polar()
+    {
+        float radius = sqrt(xco * xco + yco * yco);
+        float angleRad = atan2(yco, xco);
+        float angle = angleRad * 180 / 3.14159;
+        return Polar(radius, angle);
     }
-    void display(){
-        cout<<"The x coordinate is "<<xco <<"  and y coordinate is :"<<yco<<endl;
+
+    void display()
+    {
+        cout << "Cartesian Coordinates: (" << xco << ", " << yco << ")" << endl;
     }
 };
-Polar::operator Cartesian(){
-    float x=radius*cos(angle);
-    float y=radius*sin(angle);
-    return Cartesian(x,y);
-}
-int main() {
-    float radius, angle;
-    cout << "Enter the radius: ";
-    cin >> radius;
-    cout << "Enter the angle (in degrees): ";
-    cin >> angle;
-    Polar p(radius,angle);
-    p.display();
 
-    Cartesian cart(p);
+int main()
+{
+    Cartesian cart;
+    cart.input();
+    Polar pol = cart;
     cart.display();
-
+    pol.display();
     return 0;
 }
